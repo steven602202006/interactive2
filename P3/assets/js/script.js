a5370d41da4f2b06c808313a57a3dd5d50a4a097
Steven <wu@cca.edu>
2018-12-06 00:22:44 -0800

    good

--- P3/assets/js/script.js
+++ P3/assets/js/script.js
@@ -36,4 +36,11 @@ $(document).ready(function(){
         }
     });
 
+    $(document).bind('mousemove', function(e){
+    $('#tail').css({
+        left: e.pageX + 5,
+        top: e.pageY - 20
+    });
+});
+
 });
